64963df244c04c4179e7adc7524a0e62942d79e0
Manahil Fatima <125747158+manahil-gillani@users.noreply.github.com>
2024-07-12 18:49:27 +0500

    checking

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -4,6 +4,7 @@ import RevenueChart from '@/app/ui/dashboard/revenue-chart';
 import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
 import { lusitana } from '@/app/ui/fonts';
 import { fetchRevenue } from '@/app/lib/data';
+import { revenue } from '../lib/placeholder-data';
 
 export default async function Page() {
   return (
