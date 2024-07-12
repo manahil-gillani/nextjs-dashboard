5a4a24bd9064dbb39c269d2bc21171140b2c607c
Manahil Fatima <125747158+manahil-gillani@users.noreply.github.com>
2024-07-12 18:54:44 +0500

    okk

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -4,9 +4,10 @@ import RevenueChart from '@/app/ui/dashboard/revenue-chart';
 import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
 import { lusitana } from '@/app/ui/fonts';
 import { fetchRevenue } from '@/app/lib/data';
-import { revenue } from '../lib/placeholder-data';
+//import { revenue } from '../lib/placeholder-data';
 
 export default async function Page() {
+  const revenue = await fetchRevenue();
   return (
     <main>
       <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
